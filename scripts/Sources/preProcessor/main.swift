96e60bb6cd5e8cf2ba8d2881347a2af238e71da4
htalat <hassi.talat@gmail.com>
2020-10-05 23:16:20 -0700

    ci test

--- scripts/Sources/preProcessor/main.swift
+++ scripts/Sources/preProcessor/main.swift
@@ -2,14 +2,25 @@ import Foundation
 import Naqqash
 
 // get file URLs from ../text directory
-let textDirectoryPath = "../text/"
+let textDirectoryPath = "../tex/"
 let textDirectoryURL: URL = NSURL.fileURL(withPath: textDirectoryPath)
 let files = try! FileManager.default.contentsOfDirectory(at: textDirectoryURL,
                                                          includingPropertiesForKeys: nil,
                                                          options: [.skipsHiddenFiles])
 
 for file in files {
+    print("Processing \(file)")
+
     let contents = try! String(contentsOf: file)
+
+    // check if valid xml
+    do {
+        let _ = try XMLDocument(xmlString: contents, options: .documentIncludeContentTypeDeclaration)
+    } catch {
+        print("Bad xml \(file)")
+        continue
+    }
+
     var modified = ""
     // decompose any character that needs to be decombposed
     for char in contents {
