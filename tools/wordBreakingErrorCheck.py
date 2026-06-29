0e768be32a0bb68bbd264e452ee26026fa8299b6
Zeerak Ahmed <zeerak.ahmed@gmail.com>
2026-06-28 22:05:42 -0700

    word breaking error corrections

--- tools/wordBreakingErrorCheck.py
+++ tools/wordBreakingErrorCheck.py
@@ -1,13 +1,15 @@
 from itertools import islice
 import subprocess
+import sys
+import tty
+import termios
 
 # command line options
-prompt_string = 'Yes (y) | No (n) | Maybe (m) | Quit (q)?\n'
-yes_option = ['yes', 'y']
-no_option = ['no', 'n']
-maybe_option = ['maybe', 'm']
-quit_option = ['quit', 'q']
-acceptable_answers = yes_option + no_option + maybe_option + quit_option
+yes_option = ['y']
+no_option = ['n']
+maybe_option = ['m']
+quit_option = ['q']
+acceptable_keys = yes_option + no_option + maybe_option + quit_option
 
 # files
 input_filename = "../stats/wordBreakingErrors"
@@ -18,53 +20,72 @@ approvelist_filename = '../scripts/Sources/wordBreakingValidator/approveList'
 def next_n_lines(file_opened, N = 4):
     return [x.strip() for x in islice(file_opened, N)]
 
+def read_single_key():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(fd)
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+def clear_lines(n):
+    for _ in range(n):
+        sys.stdout.write('\x1b[1A\x1b[2K')
+
 def do_yes_things(text, suggestion):
   with open(replacements_filename, "a") as file_obj:
     file_obj.write(f"{text}\t{suggestion}\n")
-    print('word: {} added to file: {}'.format(suggestion, replacements_filename))
 
 def do_no_things(text):
   with open(approvelist_filename, "a") as file_obj:
     file_obj.write(f"{text}\n")
-    print('word: {} added to file: {}'.format(suggestion, approvelist_filename))
 
 def do_maybe_things():
   pass
 
 # run command line script
+DISPLAY_LINES = 2  # "Text/Suggestion/Frequency" line + prompt line
+
 with open(input_filename) as file_obj:
 
     # ignore first two lines of wordBreakingErrors
     next_n_lines(file_obj, 2)
 
-    # read one wordBreakingError object from the file and show options
+    first = True
     arr = next_n_lines(file_obj)
     while len(arr) == 4:
       empty_line = arr[0]
       text = arr[1].split(': ')
       suggestion = arr[2].split(': ')
       frequency = arr[3].split(': ')
-      
-      print('Text: {}, Suggestion: {}, Frequency {}'.format(text[1], suggestion[1], frequency[1]))
-      
-      answer = input(prompt_string)
-      while answer not in acceptable_answers:
-        answer = input(prompt_string)
-      
+
+      if not first:
+          clear_lines(DISPLAY_LINES)
+      first = False
+
+      print('Text: {}, Suggestion: {}, Frequency: {}'.format(text[1], suggestion[1], frequency[1]))
+      print('Yes (y) | No (n) | Maybe (m) | Quit (q)? ', end='', flush=True)
+
+      answer = read_single_key().lower()
+      while answer not in acceptable_keys:
+          answer = read_single_key().lower()
+
+      print(answer)
+
       if answer in quit_option:
         break
-      
+
       if answer in yes_option:
         do_yes_things(text[1], suggestion[1])
-      
+
       if answer in no_option:
         do_no_things(text[1])
-      
+
       if answer in maybe_option:
         do_maybe_things()
 
-      print('\n')
-      
       arr = next_n_lines(file_obj)
 
 # These next two methods are to automatically commit everytime a decision is made using the command line tools.
